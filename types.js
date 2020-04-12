3a53609f7f8b40012f55e88ab7743ec7e0c3256d
Tony Lee <hy.tony.lee@gmail.com>
2020-04-11 21:35:30 -0700

    Add variable types

--- types.js
+++ types.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+var myString;
+var myNum;
+var myBoo;
+myString = 'hello';
+myNum = 5.5;
+myBoo = true;
+console.log(myBoo);
--- types.ts
+++ types.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!')
+
+let myString: string;
+let myNum: number;
+let myBoo: boolean;
+myString = 'hello';
+myNum = 5.5;
+myBoo = true;
+
+console.log(myBoo);
+
