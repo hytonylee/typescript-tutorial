a3898c1abf11b226f7324f61e134923011ed1a15
Tony Lee <hy.tony.lee@gmail.com>
2020-04-12 09:01:26 -0700

    Add void and multiple array

--- types.js
+++ types.js
@@ -1,7 +1,24 @@
+// Simple data types
 var myString;
 var myNum;
 var myBoo;
+var myVar;
 myString = 'hello';
 myNum = 5.5;
 myBoo = true;
-console.log(myBoo);
+myVar = 5;
+// let strArr: string[];
+// let numArr: number[];
+// let booArr: boolean[];
+var strArr;
+var numArr;
+var booArr;
+strArr = ['hello', 'world'];
+numArr = [1, 3, 5];
+booArr = [true, false, true];
+// multi array
+var strMulti;
+strMulti = ["Hello", 4];
+// void
+var myVoid = null;
+console.log(myVoid);
--- types.ts
+++ types.ts
@@ -1,10 +1,32 @@
 
+// Simple data types
 let myString: string;
 let myNum: number;
 let myBoo: boolean;
+let myVar: any;
 myString = 'hello';
 myNum = 5.5;
 myBoo = true;
+myVar = 5;
 
-console.log(myBoo);
+// let strArr: string[];
+// let numArr: number[];
+// let booArr: boolean[];
+let strArr: Array<string>;
+let numArr: Array<number>;
+let booArr: Array<boolean>;
+strArr = ['hello', 'world'];
+numArr = [1, 3, 5];
+booArr = [true, false, true];
+
+// multi array
+let strMulti: [string, number];
+strMulti = ["Hello", 4];
+
+// void
+let myVoid: void = null;
+
+
+
+console.log(myVoid);
 
